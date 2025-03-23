54132ba8f3200bb4a0eab0be602f073561a51775
Slashifytech <theslashifytech@gmail.com>
2025-03-24 02:34:36 +0530

    Update TeamsRoute.js

--- routes/TeamsRoute.js
+++ routes/TeamsRoute.js
@@ -4,11 +4,12 @@ const {
   getTeamData,
   updateTeamData,
   topPerformerLists,
+    topLeadLists,
 } = require("../controllers/teamDataController");
 const { authCheck } = require("../middleware/Auth");
 
 router.get("/getTeams", authCheck,  getTeamData);
 router.patch("/update-team",  authCheck, updateTeamData);
 router.get("/top-performer-lists",  authCheck, topPerformerLists);
-
+router.get("/top-lead-performer-lists", authCheck, topLeadLists);
 module.exports = router;
