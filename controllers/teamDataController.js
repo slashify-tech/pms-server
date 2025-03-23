db5427fc076ac81415fbca756d462a9effe62426
Slashifytech <theslashifytech@gmail.com>
2025-03-24 02:33:19 +0530

    Update teamDataController.js

--- controllers/teamDataController.js
+++ controllers/teamDataController.js
@@ -45,10 +45,11 @@ exports.updateTeamData = async (req, res) => {
 exports.topPerformerLists = async (req, res) => {
   const {
     page = 1,
-    limit = 10,
+    limit = 20,
     search = "",
     location,
     teamName,
+    leadName,
     startMonth,
     endMonth,
     year,
@@ -75,6 +76,9 @@ exports.topPerformerLists = async (req, res) => {
 
     if (teamName) {
       matchConditions["teams.teamName"] = teamName;
+    }
+    if (leadName) {
+      matchConditions["teams.leadName"] = leadName;
     }
     let startDate = new Date(`${selectedYear}-01-01T00:00:00.000Z`);
     let endDate = selectedEndYear
@@ -108,6 +112,8 @@ exports.topPerformerLists = async (req, res) => {
             employeeName: "$teams.employeeName",
             teamName: "$teams.teamName",
             location: "$teams.location",
+            leadName: "$teams.leadName",
+
           },
           totalPrice: { $sum: "$totalPrice" },
           documentCount: { $sum: 1 },
@@ -119,6 +125,7 @@ exports.topPerformerLists = async (req, res) => {
           employeeName: "$_id.employeeName",
           teamName: "$_id.teamName",
           location: "$_id.location",
+          leadName: "$_id.leadName",
           totalPrice: 1,
           documentCount: 1,
         },
@@ -164,6 +171,137 @@ exports.topPerformerLists = async (req, res) => {
   }
 };
 
+
+
+
+
+exports.topLeadLists = async (req, res) => {
+  const {
+    page = 1,
+    limit = 20,
+    search = "",
+    location,
+    teamName,
+    leadRole,
+    startMonth,
+    endMonth,
+    year,
+    endYear,
+  } = req.query;
+
+  try {
+    const skip = (parseInt(page) - 1) * parseInt(limit);
+    const selectedYear = year ? parseInt(year) : new Date().getFullYear();
+    const selectedEndYear = endYear ? parseInt(endYear) : null;
+    const matchConditions = {
+      policyType: "MB",
+      isDisabled: false,
+      policyStatus: { $in: ["approved"] },
+    };
+
+    if (search) {
+      matchConditions["teams.employeeName"] = { $regex: search, $options: "i" };
+    }
+
+    if (location) {
+      matchConditions["teams.location"] = location;
+    }
+
+    if (teamName) {
+      matchConditions["teams.teamName"] = teamName;
+    }
+    if (leadRole) {
+      const roleRegex = new RegExp(`\\(\\s*${leadRole}\\s*\\)$`, "i"); // Matches role inside parentheses
+      matchConditions["teams.leadName"] = { $regex: roleRegex };
+    }
+    
+    let startDate = new Date(`${selectedYear}-01-01T00:00:00.000Z`);
+    let endDate = selectedEndYear
+      ? new Date(`${selectedEndYear}-12-31T23:59:59.999Z`)
+      : new Date(`${selectedYear}-12-31T23:59:59.999Z`);
+      if (startMonth && endMonth) {
+        const startMonthIndex = monthMapping[startMonth];
+        const endMonthIndex = monthMapping[endMonth];
+  
+        if (startMonthIndex !== undefined && endMonthIndex !== undefined) {
+          startDate = new Date(`${selectedYear}-${(startMonthIndex + 1).toString().padStart(2, '0')}-01T00:00:00.000Z`);
+          endDate = new Date(`${selectedEndYear || selectedYear}-${(endMonthIndex + 1).toString().padStart(2, '0')}-31T23:59:59.999Z`);
+        }
+      } else if (startMonth) {
+        const startMonthIndex = monthMapping[startMonth];
+        if (startMonthIndex !== undefined) {
+          startDate = new Date(`${selectedYear}-${(startMonthIndex + 1).toString().padStart(2, '0')}-01T00:00:00.000Z`);
+        }
+      } else if (endMonth) {
+        const endMonthIndex = monthMapping[endMonth];
+        if (endMonthIndex !== undefined) {
+          endDate = new Date(`${selectedEndYear || selectedYear}-${(endMonthIndex + 1).toString().padStart(2, '0')}-31T23:59:59.999Z`);
+        }
+      }
+      const pipeline = [
+        { $match: matchConditions },
+        { $unwind: "$teams" },
+        {
+          $group: {
+            _id: "$teams.leadName",
+            location: {$first: "$teams.location" },
+            teamName: {$first: "$teams.teamName"},
+            employees: { $addToSet: "$teams.employeeName" },
+            totalPolicies: { $sum: 1 },
+            totalAmount: { $sum: "$totalPrice" },
+            
+          },
+        },
+        {
+          $project: {
+            _id: 0,
+            leadName: "$_id",
+            teamName: 1,
+            location: 1,
+            employees: 1,
+            totalPolicies: 1,
+            totalAmount: 1,
+          },
+        },
+        { $sort: { totalPolicies: -1 } },
+        { $skip: skip },
+        { $limit: parseInt(limit) },
+      ];
+  
+      const data = await Policy.aggregate(pipeline);
+      
+      const totalCountPipeline = [
+        { $match: matchConditions },
+        { $unwind: "$teams" },
+        {
+          $group: {
+            _id: "$teams.leadName",
+          },
+        },
+        { $count: "totalCount" },
+      ];
+  
+      const totalCountResult = await Policy.aggregate(totalCountPipeline);
+      const totalCount = totalCountResult[0]?.totalCount || 0;
+  
+      return res.status(200).json({
+        message: "Data fetched successfully",
+        data,
+        pagination: {
+          currentPage: parseInt(page),
+          totalPages: Math.ceil(totalCount / limit),
+          totalItems: totalCount,
+        },
+      });
+    } catch (error) {
+      console.error("Error fetching top lead lists:", error);
+      return res.status(500).json({
+        message: "Something went wrong",
+        error: error.message,
+      });
+    }
+  };
+  
 
 exports.downloadTopPerformerCsv = async (req, res) => {
   try {
